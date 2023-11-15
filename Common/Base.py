c2b8a40fad630dab8d398acf7fbbdb61c7c04f53
Brian <zxy51320@gmail.com>
2023-11-15 00:14:53 -0500

    update

--- Common/Base.py
+++ Common/Base.py
@@ -2,6 +2,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
 import time
 
 class BasePage():
@@ -21,31 +23,102 @@ class BasePage():
             return False
 
     def input_text(self, *loc, text):
-        __element = self.wait.until(EC.element_to_be_clickable(*loc))
+        __element = self.scroll_to_element(*loc)
         __element.send_keys(text)
 
-    def scroll_to(self, *loc):
+    def scroll_to_element(self, *loc):
         __element = self.driver.find_element(*loc)
         self.driver.execute_script("arguments[0].scrollIntoView();", __element)
         time.sleep(1)
+        return __element
 
     def click(self, *loc):
-        __element = self.wait.until(EC.element_to_be_clickable(*loc))
+        __element = self.scroll_to_element(*loc)
         __element.click()
 
     def get_text(self, *loc):
         __text = self.wait.until(EC.presence_of_element_located(*loc)).text
         return __text
     
-    def check_url(self,content):
+    def check_url(self, content:str):
         try:
             self.wait.until(EC.url_contains(content))
             return True
         except TimeoutException:
             return False
 
-    def switch_handle(self,window_name):
-        self.driver.switch_to.window(window_name)
+    def create_new(self, param:str):
+        """
+        Parameters:
+            param (str): 操作类型，可以是 'tab' 或 'window'。
+        """
+        self.driver.switch_to.new_window(f'{param}')
+    
+    def get_all_windows(self):
+        __all_windows = self.driver.window_handles
+        return __all_windows
+    
+    def get_current_window(self):
+        __current_window = self.driver.current_window_handle
+        return __current_window
+    
+    def switch_window(self, window_handle):
+        self.driver.switch_to.window(window_handle)
+
+    def alert_handle(self, action:str):
+        """
+        处理 JavaScript 弹窗(alert)。
+
+        Parameters:
+            action (str): 操作类型，可以是 'accept' 或 'dismiss'。
+        """
+        __alert = self.driver.switch_to.alert
+        if action == 'accept':
+            __alert.accept()
+        elif action == 'dismiss':
+            __alert.dismiss()
+        else:
+            pass
 
     def get_title(self):
         return self.driver.title
+    
+    def select_option(self, *loc, method:str, value: str|int)->str:
+        """
+        选择下拉框或列表项。
+
+        Parameters:
+            method (str): 匹配类型，可以是 'text', 'value' 或 'index'。
+        """
+        __element = self.wait.until(EC.element_to_be_clickable(*loc))
+        __option_list = self.Select(__element)
+        match method:
+            case 'text':
+                __option_list.select_by_visible_text(f'{value}')
+            case 'value':
+                __option_list.select_by_value(value)
+            case 'index':
+                __option_list.select_by_index(value)
+            case _:
+                pass
+        
+    def doubleClick(self, *loc):
+        __element = self.wait.until(EC.element_to_be_clickable(*loc))
+        __action = ActionChains(self.driver)
+        __action.double_click(__element).perform()
+        
+    def mouse_pointing(self, *loc):
+        __element = self.scroll_to_element(*loc)
+        __action = ActionChains(self.driver)
+        __action.move_to_element(__element).perform()
+        
+    def right_click(self, *loc):
+        __element = self.wait.until(EC.element_to_be_clickable(*loc))
+        __action = ActionChains(self.driver)
+        __action.context_click(__element).perform()
+    
+    def drag_and_drop(self, *loc):
+        __element1 = self.wait.until(EC.element_to_be_clickable(*loc[0]))
+        __element2 = self.wait.until(EC.element_to_be_clickable(*loc[1]))
+        __action = ActionChains(self.driver)
+        __action.drag_and_drop(__element1,__element2).perform()
--- Page_Objects/Login_Page.py
+++ Page_Objects/Login_Page.py
@@ -4,27 +4,26 @@ import time
  
 class LoginPage(BasePage):
 
-    username_input = 'ion-input-0'
-    pwd_input = 'ion-input-1'
-    login_btn = "//ion-button[@type='submit']"
+    username_input = (By.NAME,'ion-input-0')
+    pwd_input = (By.NAME,'ion-input-1')
+    login_btn = (By.XPATH,"//ion-button[@type='submit']")
     main_page = 'user-profile'
-    user_icon = "//ion-avatar[@class='padded-avatar md hydrated']"
-    logout = "//ion-label[@class='sc-ion-label-md-h sc-ion-label-md-s md hydrated' and text()='Logout']"
+    user_icon = (By.XPATH,"//ion-avatar[@class='padded-avatar md hydrated']")
+    logout = (By.XPATH,"//ion-label[@class='sc-ion-label-md-h sc-ion-label-md-s md hydrated' and text()='Logout']")
 
     def __init__(self,driver):
         super().__init__(driver)
         
     def _login(self, usr, pwd):
-        self.input_text((By.NAME,self.username_input), text=usr)
-        self.scroll_to(By.NAME,self.pwd_input)
-        self.input_text((By.NAME,self.pwd_input), text=pwd)
-        self.click((By.XPATH,self.login_btn))
+        self.input_text(*self.username_input, text=usr)
+        self.input_text(*self.pwd_input, text=pwd)
+        self.click(*self.login_btn)
 
     def _login_result(self):
         __result = self.check_url(self.main_page)
         return __result
         
     def _logout(self):
-        self.click((By.XPATH,self.user_icon))
-        self.click((By.XPATH,self.logout))
+        self.click(*self.user_icon)
+        self.click(*self.logout)
         time.sleep(1)
